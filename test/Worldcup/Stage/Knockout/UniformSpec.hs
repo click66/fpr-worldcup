bd4f4b9b0b4e4bae61f18fc9d9e8718adc260300
Clark Sirl <click66@gmail.com>
2018-10-17 13:56:39 +0100

    Added additional property tests around uniform function

--- test/Worldcup/Stage/Knockout/UniformSpec.hs
+++ test/Worldcup/Stage/Knockout/UniformSpec.hs
@@ -1,9 +1,11 @@
 module Worldcup.Stage.Knockout.UniformSpec (spec) where
 
+import Data.Ratio
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
 import Worldcup.Stage.Knockout (uniform)
+import Worldcup.SpecHelpers
 
 spec :: Spec
 spec = do
@@ -12,4 +14,10 @@ spec = do
         it "produces a uniform distribution summing to 1 when given a non-empty list" $ property $
             let totalProbability ((_, p):xs) = p + (totalProbability xs)
                 totalProbability [] = 0
-            in (\(NonEmpty xs) -> (totalProbability $ uniform (xs :: [Int])) `shouldBe` 1)
+            in \(NonEmpty xs) -> (totalProbability $ uniform (xs :: [Int])) `shouldBe` 1
+
+        it "produces a list where each probability is equal to 1 divided by the length of the supplied list" $ property $
+            let isProbability (_, p) ep = p == ep
+                allExpected []     ep = True
+                allExpected (d:ds) ep = (isProbability d ep) && (allExpected ds ep)
+            in \(NonEmpty xs) -> allExpected (uniform (xs :: [Int])) (toRational (1 % length xs))
